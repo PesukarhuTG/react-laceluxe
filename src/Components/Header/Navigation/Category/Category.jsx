ec29f6dba32ed62ab776163561d15cbdc35a6812
PesukarhuTG <ganzhinovat@yandex.ru>
2023-07-09 20:05:59 +0300

    refactor: output categories in header

--- src/Components/Header/Navigation/Category/Category.jsx
+++ src/Components/Header/Navigation/Category/Category.jsx
@@ -1,15 +1,19 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import style from './Category.module.scss';
 import cn from 'classnames';
 
 const Category = ({list}) => {
-  console.log(list);
+  const location = useLocation();
+  const gender = location.pathname.split('/')[1] || 'women';
+
+  const categoriesList = list.find(item => item.link === gender);
+
   return (
     <ul className={style.category}>
-      {list[0].categories.map((item) => {
+      {categoriesList.categories.map((item) => {
         return (
           <li key={item.link}>
-            <NavLink className={({isActive}) => cn(style.link, isActive && style.linkActive )} to={`${list[0].link}/${item.link}`}>{item.title}</NavLink>
+            <NavLink className={({isActive}) => cn(style.link, isActive && style.linkActive )} to={`${gender}/${item.link}`}>{item.title}</NavLink>
           </li>
         );
       })}
--- src/Components/Header/Navigation/Gender/Gender.jsx
+++ src/Components/Header/Navigation/Gender/Gender.jsx
@@ -1,14 +1,27 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import style from './Gender.module.scss';
 import cn from 'classnames';
 
 const Gender = ({ list }) => {
+  const location = useLocation();
+  const gender = location.pathname.split('/')[1] || 'women';
+
   return (
     <ul className={style.gender}>
       {list.map((item) => {
         return (
           <li className={style.item} key={item.link}>
-            <NavLink className={({isActive}) => cn(style.link, isActive && style.linkActive )} to={item.link}>{item.title}</NavLink>
+            <NavLink
+              className={({ isActive }) =>
+                cn(
+                  style.link,
+                  (isActive || gender === item.link) && style.linkActive
+                )
+              }
+              to={item.link}
+            >
+              {item.title}
+            </NavLink>
           </li>
         );
       })}
