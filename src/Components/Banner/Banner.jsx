ccff14cc66e0847d87dca42f849e2371c31305d4
PesukarhuTG <ganzhinovat@yandex.ru>
2023-07-13 11:56:57 +0300

    feat: use react-use library for change image banner according to screen sizes

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -2,13 +2,32 @@ import { NavLink } from 'react-router-dom';
 import Container from '../Layout/Container/Container';
 import style from './Banner.module.scss';
 import { API_URL } from '../../const';
+import { useMedia } from 'react-use';
+import { useEffect, useState } from 'react';
 
 const Banner = ({ data }) => {
+  const isMobile = useMedia('(max-width: 540px');
+  const isTablet = useMedia('(max-width: 768px');
+  const isLaptop = useMedia('(max-width: 1024px');
+  const [bgURL, setBgURL] = useState('');
+
+  useEffect(() => {
+    if (isMobile) {
+      setBgURL(`${API_URL}/${data?.bg.mobile}`);
+    } else if (isTablet) {
+      setBgURL(`${API_URL}/${data?.bg.tablet}`);
+    } else if (isLaptop) {
+      setBgURL(`${API_URL}/${data?.bg.laptop}`);
+    } else {
+      setBgURL(`${API_URL}/${data?.bg.desktop}`);
+    }
+  }, [isMobile, isTablet, isLaptop, data]);
+
   return (
     data && (
       <section
         className={style.banner}
-        style={{ backgroundImage: `url(${API_URL}/${data.bg.desktop})` }}
+        style={{ backgroundImage: `url(${bgURL})` }}
       >
         <Container>
           <div className={style.content}>
